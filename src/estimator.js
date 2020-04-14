7ff5408820b500acda873df3470d94606baf5520
Claudgonye <claudgonyek@gmail.com>
2020-04-14 17:40:02 +0300

    first

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return{
+        data: input,
+        impact: {},
+        severeimpact: {}
+    };
+};
+data = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+    }
+currentlyInfected = reportedCases;
+impact.currentlyInfected = reportedCases * 10;
+severeImpact.currentlyInfected = reportedCases * 50;
+
 
 export default covid19ImpactEstimator;
